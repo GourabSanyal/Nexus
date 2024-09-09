835dca7d1e3a99104282599d14f6fe3e4b31ce05
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-09-09 17:22:17 +0530

    ui - wallet header comp create

---
+++ apps/web/app/components/ui/header/WalletHeader.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "../button";
+import { Sun, Moon } from "lucide-react";
+
+interface WalletHeaderProps {
+  toggleTheme: () => void;
+  isDarkMode: boolean;
+}
+
+function WalletHeader({ toggleTheme, isDarkMode }: WalletHeaderProps) {
+  return (
+    <div className="flex flex-col sm:flex-row justify-between items-center mb-8">
+      <h1 className="text-2xl font-bold mb-4 sm:mb-0">Nexus</h1>
+      <Button onClick={toggleTheme} size="icon" className="rounded-full p-2">
+        {isDarkMode ? (
+          <Sun className="h-5 w-5" />
+        ) : (
+          <Moon className="h-5 w-5" />
+        )}
+      </Button>
+    </div>
+  );
+}
+
+export default WalletHeader;
