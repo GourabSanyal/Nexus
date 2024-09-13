82112bce7a9314d93d92780a71435ba8809c3f4e
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-09-13 22:53:15 +0530

    feat - eth wallet comp init

---
+++ apps/web/app/components/ui/wallet/EthereumWallet.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface EthereumWalletProps {
+  mneumonic: string
+}
+const EthereumWallet = ({ mneumonic }: EthereumWalletProps) => {
+
+return (
+    <div>EthereumWallet
+    <div className="div">
+    
+    {/* <SingleWallet /> */}
+    </div>
+    </div>
+        
+  )
+}
+
+
+export default EthereumWallet
