1316364b6dc1b4a9703785046162c52997ca3598
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-09-19 12:19:43 +0530

    feat - button render for individual wallets

--- apps/web/app/components/ui/wallet/SingleWallet.tsx
+++ apps/web/app/components/ui/wallet/SingleWallet.tsx
@@ -57,12 +57,21 @@ const SingleWallet = ({ mnemonic, path }: SingleWalletProps) => {
   return (
     <>
       {mnemonic ? (
+        path === "60" ? (
           <Button
             onClick={generateWallet}
             className="w-full sm:w-auto mb-6 py-3 px-6 bg-primary text-primary-foreground font-semibold rounded-lg hover:bg-primary/90 transition-colors duration-300"
           >
             <Plus className="inline-block mr-2 h-5 w-5" /> Generate SOL Wallet
           </Button>
+        ) : path === "501" ? (
+          <Button
+            onClick={generateWallet}
+            className="w-full sm:w-auto mb-6 py-3 px-6 bg-primary text-primary-foreground font-semibold rounded-lg hover:bg-primary/90 transition-colors duration-300"
+          >
+            <Plus className="inline-block mr-2 h-5 w-5" /> Generate ETH Wallet
+          </Button>
+        ) : null
       ) : null}
       {wallets.map((wallet) => (
         <motion.div
