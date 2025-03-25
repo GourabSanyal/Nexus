1032938ba46ebbc90354d1d88997e829e5a6c2ce
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-03-25 12:40:03 +0530

    turbopack - turbo pack resolved extentions add

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,19 @@
 import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 const nextConfig = {
+  experimental: {
+    turbo: {
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
   reactStrictMode: true,
   transpilePackages: ["store", "zod"],
   webpack: (config) => {
