8c95b01db59aa9c8564f52f9ff87e0c8106b29cb
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-09-30 23:56:47 +0530

    feat: export additional wallet state atoms and persistence configuration

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,1 +1,7 @@
 export { walletState } from "./atoms/walletState";
+
+export * from "./atoms/solanaWalletState";
+export * from "./atoms/ethereumWalletState";
+
+// Persistence configuration
+export * from "./persistence/recoilPersistConfig";
