6eeb5c2904131ff8098855625dfdd1c13aa3ecba
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-06 18:45:45 +0530

    feat : modualrised wallte generation for sol and eth wallets

---
+++ apps/web/app/lib/utils/walletGeneration.ts
@@ -0,0 +1,24 @@
+import { WalletPath } from "@/app/constants/wallet";
+import { generateSolanaWallet } from "@/app/lib/utils/generateSolanaWallet";
+import { generateEthWallet } from "@/app/lib/utils/generateEthWallet";
+
+export const generateWallet = async (mnemonic: string, path: string) => {
+  switch (path) {
+    case WalletPath.SOLANA: {
+      const { solPublicKey, solSecretKey } = await generateSolanaWallet({
+        mnemonic,
+        path,
+      });
+      return { type: "solana" as const, publicKey: solPublicKey, privateKey: solSecretKey };
+    }
+    case WalletPath.ETHEREUM: {
+      const { ethPublicKey, ethPrivateKey } = await generateEthWallet({
+        mnemonic,
+        path,
+      });
+      return { type: "ethereum" as const, publicKey: ethPublicKey, privateKey: ethPrivateKey };
+    }
+    default:
+      throw new Error("Invalid wallet path provided");
+  }
+};
