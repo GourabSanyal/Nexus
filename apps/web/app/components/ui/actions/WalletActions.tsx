e1c54e5277ea244645e26cbf01a4f423468cfb8e
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-13 12:22:16 +0530

    feat : updated wallet actions UI to handle import wallet feature

--- apps/web/app/components/ui/actions/WalletActions.tsx
+++ apps/web/app/components/ui/actions/WalletActions.tsx
@@ -1,11 +1,23 @@
 import { WalletActionButton } from "./WalletActionButton";
 import { WalletActionsProps } from "@/app/types/wallet/WalletActionTypes";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@components/ui/tooltip";
 
 export const WalletActions = ({ generateWallet, importWallet }: WalletActionsProps) => {
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center w-full gap-4 sm:gap-6 mb-6 sm:mb-8">
       <WalletActionButton actionType="generate" onClick={generateWallet} />
-      <WalletActionButton actionType="import" onClick={importWallet} />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className="cursor-not-allowed">
+              <WalletActionButton actionType="import" onClick={importWallet} className="pointer-events-none" />
+            </span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Coming soon...</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 };
