2cf21b40608cdb6a2df92a318106ef9d57073d1c
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-22 14:46:58 +0530

    bug: fixe import for wallet action configs in WalletActionButton

--- apps/web/app/components/ui/actions/WalletActionButton.tsx
+++ apps/web/app/components/ui/actions/WalletActionButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../button/button";
 import { WalletActionButtonProps } from "@/app/types/wallet/WalletActionTypes";
-import { WALLET_ACTION_CONFIG } from "@/app/constants/wallet";
+import { WALLET_ACTION_CONFIG } from "@repo/constants/WalletActionConfig";
 
 export const WalletActionButton = ({
   actionType,
