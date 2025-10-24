2c3b2fecbbf28733202a1d28cc6f9758baa92521
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-24 17:58:02 +0530

    feat: add persistance in walletBalanceState

--- packages/store/src/atoms/walletBalancesState.ts
+++ packages/store/src/atoms/walletBalancesState.ts
@@ -1,8 +1,10 @@
 import { atom } from "recoil";
+import { persistAtom } from '../persistence/recoilPersistConfig';
 
-export const walletBalancesState = atom<Record<string, number>>({
+export const walletBalancesState = atom<Record<string, string | BigInt>>({
   key: "walletBalancesState",
   default: {},
+  effects_UNSTABLE: [persistAtom],
 });
 
 
