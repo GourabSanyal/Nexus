cb47fd33e9967df2d9303167559f23b4504b44fd
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-24 17:47:51 +0530

    chore: remove network param in checkMissingParam middleware

--- packages/api/src/utils/checkMissingParams.ts
+++ packages/api/src/utils/checkMissingParams.ts
@@ -1,12 +1,11 @@
 import { BalanceParams } from "@api-types/BalanceParams";
 
-export const checkMissingParams = (params: BalanceParams ) => {
+export const checkMissingParams = (params: BalanceParams) => {
   const missingParams: string[] = [];
 
   if (!params.chain) missingParams.push("chain");
   if (!params.address) missingParams.push("address");
   if (!params.cluster) missingParams.push("cluster");
-  if (!params.network) missingParams.push("network");
 
   if (missingParams.length > 0) {
     throw new Error(`Missing required parameters: ${missingParams.join(", ")}`);
