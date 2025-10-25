202c94f02c4b358669000517ffab3de106858487
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-25 19:16:26 +0530

    bug: update getBalance to better propagate api error to the client parent component

--- packages/api/src/services/wallet/index/getBalance.ts
+++ packages/api/src/services/wallet/index/getBalance.ts
@@ -4,18 +4,13 @@ import { checkMissingParams } from "@api-utils/checkMissingParams";
 import { getEthBalance } from "@api-service/ethereum/getEthBalance";
 
 export const getBalance = async (params: BalanceParams) => {
-  try {
-    checkMissingParams(params);
+  checkMissingParams(params);
 
-    if (params.chain === "solana") {
-      return getSolBalance(params);
-    } else if (params.chain === "ethereum") {
-      return getEthBalance(params);
-    } else {
-      throw new Error("Unsupported chain: " + params.chain);
-    }
-  } catch (error) {
-    console.error("Error fetching Solana balance:", error);
-    throw error;
+  if (params.chain === "solana") {
+    return getSolBalance(params);
+  } else if (params.chain === "ethereum") {
+    return getEthBalance(params);
+  } else {
+    throw new Error("Unsupported chain: " + params.chain);
   }
 };
