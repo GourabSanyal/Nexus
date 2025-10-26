66e011a229c2b93c3072149d6c75deda5f915a9e
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-26 23:22:59 +0530

    refactor: update getBalance to use ChainEnum for chain identification

--- packages/api/src/services/wallet/index/getBalance.ts
+++ packages/api/src/services/wallet/index/getBalance.ts
@@ -2,13 +2,14 @@ import { BalanceParams } from "@api-types/BalanceParams";
 import { getSolBalance } from "@api-service/solana/getSolBalance";
 import { checkMissingParams } from "@api-utils/checkMissingParams";
 import { getEthBalance } from "@api-service/ethereum/getEthBalance";
+import { ChainEnum } from "@repo/store/src/enums/network"
 
 export const getBalance = async (params: BalanceParams) => {
   checkMissingParams(params);
 
-  if (params.chain === "solana") {
+  if (params.chain === ChainEnum.Solana) {
     return getSolBalance(params);
-  } else if (params.chain === "ethereum") {
+  } else if (params.chain === ChainEnum.Ethereum) {
     return getEthBalance(params);
   } else {
     throw new Error("Unsupported chain: " + params.chain);
