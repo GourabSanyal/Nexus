a95923d25b78fe08f1196b83ff99e49bef8eddf8
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-10-27 18:10:37 +0530

    feat: enhance PasswordInput to render both dialog and form together

--- apps/web/app/components/ui/wallet/sections/password/PasswordInput.tsx
+++ apps/web/app/components/ui/wallet/sections/password/PasswordInput.tsx
@@ -11,6 +11,11 @@ import {
 } from "../../../form/form";
 import { Input } from "../../../input";
 import { Button } from "../../../button/button";
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "../../../dialog/dialog";
 
 type PasswordInputProps = {
   onSubmit: (data: { password: string }) => void;
@@ -22,40 +27,43 @@ const PasswordInput = ({ onSubmit }: PasswordInputProps) => {
     defaultValues: { password: "" },
   });
 
-  const onPasswordSubmit = (data: { password: string }) => {
-    onSubmit(data);
+  const onPasswordSubmit = ({ password }: { password: string }) => {
+    onSubmit({ password });
     form.reset();
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onPasswordSubmit)}>
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Enter password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-end mt-4">
-          <Button
-            type="submit"
-            className="py-2 px-4 bg-destructive text-destructive-foreground font-semibold rounded-lg hover:bg-destructive/90 transition-colors duration-300"
-          >
-            Confirm
-          </Button>
-        </div>
-      </form>
-    </Form>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle className="text-lg font-semibold">
+          Enter Password
+        </DialogTitle>
+      </DialogHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onPasswordSubmit)}>
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input type="password" placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end mt-4">
+            <Button
+              type="submit"
+              className="py-2 px-4 bg-destructive text-destructive-foreground font-semibold rounded-lg hover:bg-destructive/90 transition-colors duration-300"
+            >
+              Confirm
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </DialogContent>
   );
 };
 
