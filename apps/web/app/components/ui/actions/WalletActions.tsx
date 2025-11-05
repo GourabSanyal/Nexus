4498e70c89b7104578d3adf9515cffe12986a1f9
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-11-05 23:08:11 +0530

    feat: update WalletAcitons button with modularised WalletButton and prop hadnling for import and generate wallet

--- apps/web/app/components/ui/actions/WalletActions.tsx
+++ apps/web/app/components/ui/actions/WalletActions.tsx
@@ -1,23 +1,14 @@
 import { WalletActionButton } from "./WalletActionButton";
 import { WalletActionsProps } from "@/app/types/wallet/WalletActionTypes";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@components/ui/tooltip";
 
-export const WalletActions = ({ generateWallet, importWallet }: WalletActionsProps) => {
+export const WalletActions = ({
+  generateWallet,
+  importWallet,
+}: WalletActionsProps) => {
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center w-full gap-4 sm:gap-6 mb-6 sm:mb-8">
       <WalletActionButton actionType="generate" onClick={generateWallet} />
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <span className="cursor-not-allowed">
-              <WalletActionButton actionType="import" onClick={importWallet} className="pointer-events-none" />
-            </span>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Coming soon...</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <WalletActionButton actionType="import" onClick={importWallet} />
     </div>
   );
 };
