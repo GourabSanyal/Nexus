9c693ed99030ba2e63439ce2eeef0e3b921e00da
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-11-10 01:15:23 +0530

    feat: update rustApiClient to propagate no internet error

--- packages/api/src/utils/rustApiClient.ts
+++ packages/api/src/utils/rustApiClient.ts
@@ -1,13 +1,31 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosError } from "axios";
+import { checkInternet } from "@repo/api/src/services/shared/checkInternet";
+import { NetworkConnectionEnum } from "@repo/store/src/enums/network";
 
 const RUST_API_BASE_URL = process.env.NEXT_PUBLIC_RUST_API_URL;
 
 export const rustApiClient = (): AxiosInstance => {
-  return axios.create({
+  const client = axios.create({
     baseURL: RUST_API_BASE_URL,
     headers: {
       "Content-Type": "application/json",
     },
   });
+
+  client.interceptors.request.use(
+    (config) => {
+      if (!checkInternet()) {
+        return Promise.reject(
+          new Error(NetworkConnectionEnum.NoInternet)
+        );
+      }
+      return config;
+    },
+    (error: AxiosError) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return client;
 };
 
