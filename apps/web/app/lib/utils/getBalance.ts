edca8f51a93d8fa8e4cc12d754f40dbce2ca60e1
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-11-10 17:46:32 +0530

    migration: moved getSolanaBalance from server to client

---
+++ apps/web/app/lib/utils/getBalance.ts
@@ -0,0 +1,18 @@
+import { BalanceParams } from "@api-types/BalanceParams";
+import { ChainEnum } from "@repo/store/src/enums/network";
+import { getSolBalance } from "./getSolBalance";
+import { getEthBalance } from "@repo/api/src/services/wallet/ethereum/getEthBalance";
+import { checkMissingParams } from "@repo/api/src/utils/checkMissingParams";
+
+export const getBalance = async (params: BalanceParams) => {
+  checkMissingParams(params);
+
+  if (params.chain === ChainEnum.Solana) {
+    return getSolBalance(params);
+  } else if (params.chain === ChainEnum.Ethereum) {
+    return getEthBalance(params);
+  } else {
+    throw new Error("Unsupported chain: " + params.chain);
+  }
+};
+
--- apps/web/app/lib/utils/getSolBalance.ts
+++ apps/web/app/lib/utils/getSolBalance.ts
@@ -1,16 +1,12 @@
 import { BalanceParams } from "@api-types/BalanceParams";
 import { rustApiClient } from "@api-utils/rustApiClient";
 
-export const getSolBalance = async ({
-  address,
-  cluster,
-}: BalanceParams) => {
+export const getSolBalance = async ({ address, cluster }: BalanceParams) => {
   const client = rustApiClient();
   const requestData = {
     address,
     cluster: cluster as string,
   };
   const response = await client.post("/wallet/solana/balance", requestData);
-  // console.log("res ", response?.data)
   return response.data?.balance as number;
 };
