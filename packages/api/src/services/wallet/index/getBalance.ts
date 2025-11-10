b7560f83c72a8d776ccacbda12cd129fe28823c3
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-11-10 17:54:47 +0530

    migration: updated getBalance in packages to handle eth only

--- packages/api/src/services/wallet/index/getBalance.ts
+++ packages/api/src/services/wallet/index/getBalance.ts
@@ -1,5 +1,4 @@
 import { BalanceParams } from "@api-types/BalanceParams";
-import { getSolBalance } from "@api-service/solana/getSolBalance";
 import { checkMissingParams } from "@api-utils/checkMissingParams";
 import { getEthBalance } from "@api-service/ethereum/getEthBalance";
 import { ChainEnum } from "@repo/store/src/enums/network"
@@ -7,9 +6,8 @@ import { ChainEnum } from "@repo/store/src/enums/network"
 export const getBalance = async (params: BalanceParams) => {
   checkMissingParams(params);
 
-  if (params.chain === ChainEnum.Solana) {
-    return getSolBalance(params);
-  } else if (params.chain === ChainEnum.Ethereum) {
+
+  if (params.chain === ChainEnum.Ethereum) {
     return getEthBalance(params);
   } else {
     throw new Error("Unsupported chain: " + params.chain);
