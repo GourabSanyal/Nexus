f3253074551d0791a395959c7f302002a9685895
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-11-13 17:47:45 +0530

    refactor: restructure RPC_ENDPOINTS to use a getter for Ethereum configurations

--- packages/api/src/configs/rpcConfigs.ts
+++ packages/api/src/configs/rpcConfigs.ts
@@ -1,10 +1,9 @@
 export const RPC_ENDPOINTS = {
-  ethereum: {
-    mainnet:
-    process.env.ETHEREUM_MAINNET,
-    sepolia:
-    process.env.ETHEREUM_SEPOLIA,
-    holesky:
-    process.env.ETHEREUM_HOLESKY,
+  get ethereum() { 
+    return {
+      mainnet: process.env.ETHEREUM_MAINNET,
+      sepolia: process.env.ETHEREUM_SEPOLIA,
+      holesky: process.env.ETHEREUM_HOLESKY,
+    };
   },
 };
