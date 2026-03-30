a8a00f30d00852b1cc39a243c0bbe9801a926519
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-03-30 22:53:24 +0530

    feat: update recieve modal props to handle different chains for transactions

--- apps/web/app/types/wallet/ReceiveModalProps.ts
+++ apps/web/app/types/wallet/ReceiveModalProps.ts
@@ -1,5 +1,9 @@
+import { ChainEnum, NetworkEnum } from "@repo/store/src/enums/network";
+
 export interface ReceiveModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    publicKey: string;
-  }
+  isOpen: boolean;
+  onClose: () => void;
+  publicKey: string;
+  chain: ChainEnum;
+  network: NetworkEnum;
+}
