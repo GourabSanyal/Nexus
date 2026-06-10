eb91aab01cb233d5fc3b0123e643a058d0101f0b
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-06-11 00:02:02 +0530

    style(rust-apis): reorder wallet module exports

--- packages/rust-apis/src/api/wallet/mod.rs
+++ packages/rust-apis/src/api/wallet/mod.rs
@@ -6,6 +6,6 @@ mod send;
 mod transactions;
 
 pub use balance::handle_balance;
+pub use import::handle_import_data;
 pub use send::{handle_prepare_send, handle_send};
 pub use transactions::handle_transactions;
-pub use import::handle_import_data;
