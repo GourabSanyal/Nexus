dce45d41255541a4e37a7045d695d0b43f3af806
Gourab Sanyal <sanyal.gourab@gmail.com>
2026-06-10 23:52:44 +0530

    refactor(zod): export import preview schemas from package index

--- packages/zod/src/index.ts
+++ packages/zod/src/index.ts
@@ -1,4 +1,5 @@
-export * from "./walletSchemas/walletSchema"
-export * from "./walletSchemas/importWalletSchema"
+export * from "./walletSchemas/walletSchema";
+export * from "./walletSchemas/importWalletSchema";
+export * from "./walletSchemas/importPreviewSchema";
 export * from "./password/privateKey"
 export * from "./send/sendSchema"
